c41a276c3e00b7467f6cc1e1952340df22d31b86
flyin <flying.falcon@outlook.com>
2024-02-16 17:12:13 +0100

    Template Driven Forms Custom Field Validator - a Password Strength Validator: start working on.

---
+++ src/app/validators/password-strength.validator.ts
@@ -0,0 +1,9 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
+
+
+export function createPasswordStrengthValidator(): ValidatorFn {
+  return (control: AbstractControl) : ValidationErrors | null => {
+
+    return null;
+  }
+}
