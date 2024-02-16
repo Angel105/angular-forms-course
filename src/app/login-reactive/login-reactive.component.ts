c6a2ffc63eddb881aac236d04c9c7271c898b640
flyin <flying.falcon@outlook.com>
2024-02-16 20:14:32 +0100

    The Form Builder API - Writing much more concise Reactive Forms: use fb (FormBuilder) to build your form in a more concise way. Use array of validators to place validators in configuration object for 'validators' field and updateOn 'blur' for email. Place validators array inside configuration array for 'password' form control. All validators are synchronous in this example. Use formControlName attribute in 'html' to link to form control names in 'ts' file. Add password strength validator taken from template-driven forms branch.

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {createPasswordStrengthValidator} from "../validators/password-strength.validator";
 
 
 @Component({
@@ -9,25 +10,15 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class LoginReactiveComponent implements OnInit {
 
-  email = new FormControl('', {
-    validators: [Validators.required, Validators.email],
-    updateOn: 'blur'
-  });
-  password = new FormControl('', {
-    validators: [
-      Validators.required,
-      Validators.minLength(8),
-      // createPasswordStrengthValidator() // TODO define a custom-validator function and plug it in our form control
-    ]
-  });
-  form = new FormGroup({
-    email: this.email,
-    password: this.password
+  form = this.fb.group({
+    email: ['', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'
+    }],
+    password: ['', [Validators.required, Validators.minLength(8), createPasswordStrengthValidator()]]
   });
 
-  constructor() {
-
-
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
