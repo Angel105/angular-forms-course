af9e93dc1ca581f88a18528197a36de6f9cb6c80
flyin <flying.falcon@outlook.com>
2024-02-18 16:33:36 +0100

    Angular Reactive Forms - Implementing Form Draft Pre-Save

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, Validators} from '@angular/forms';
 import {courseTitleValidator} from "../../validators/course-title.validator";
 import {CoursesService} from "../../services/courses.service";
 import {Observable} from "rxjs";
+import {filter} from "rxjs/operators";
 
 
 interface CourseCategory {
@@ -37,6 +38,16 @@ export class CreateCourseStep1Component implements OnInit {
 
   ngOnInit() {
     this.courseCategories$ = this.courses.findCourseCategories();
+
+    const draft = localStorage.getItem("STEP_1");
+
+    if (draft) {
+      this.form.setValue(JSON.parse(draft));
+    }
+
+    this.form.valueChanges.pipe(
+      filter(()=>this.form.valid)
+    ).subscribe(val => localStorage.setItem("STEP_1", JSON.stringify(val)));
   }
 
   get courseTitle() {
