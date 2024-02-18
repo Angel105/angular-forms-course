444b235ed7465731fb90deec6522063f0175f40d
flyin <flying.falcon@outlook.com>
2024-02-18 19:35:56 +0100

    Implementing a File Upload Progress Indicator: add a configuration object to post method with reportProgress set to 'true' and to observe 'events' (also by default), add a finalize section to the pipe that will be executed anyway to set uploadProgress to null, and check in the subscribe method the type of event whether it is UploadProgress and calculate the uploadProgress percentage to be set as [value] is the mat-progress-bar.

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
+import {HttpClient, HttpEventType} from '@angular/common/http';
+import {catchError, finalize} from 'rxjs/operators';
 import {of} from 'rxjs';
 
 
@@ -17,6 +17,7 @@ export class FileUploadComponent {
   fileName = '';
 
   fileUploadError = false;
+  uploadProgress: number;
 
   constructor(private http: HttpClient) {
   }
@@ -33,15 +34,25 @@ export class FileUploadComponent {
 
       this.fileUploadError = false;
 
-      this.http.post("/api/thumbnail-upload", formData)
+      this.http.post("/api/thumbnail-upload", formData, {
+        reportProgress: true,
+        observe: 'events'
+      })
         .pipe(
           catchError(error=> {
             this.fileUploadError = true;
 
             return of(error);
+          }),
+          finalize(() => {
+            this.uploadProgress = null;
           })
         )
-        .subscribe();
+        .subscribe(event => {
+          if (event.type == HttpEventType.UploadProgress) {
+            this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+          }
+        });
     }
 
   }
